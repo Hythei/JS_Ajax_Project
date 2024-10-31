32e89a2929d5cae672ab3d545f141dafcdb19760
HytHei <heikki.hyttinen16@gmail.com>
2024-10-31 21:34:26 +0200

    added a proper UI, though still WIP, error validation, and added some more information that the script fetches.

--- script.js
+++ script.js
@@ -33,39 +33,65 @@ function searchBooks(authorInfo, title_info, genre){
         .then(data => {
             const books = data.items;
             displayBooks(books);
+            let error = document.querySelector("#error_message");
+            if (error){
+                error.remove();
+            }
+            resetFields();
         })
-        .catch(error => console.error('Error fetching books:', error));
+        
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            ErrorMessages();
+        });
 }
 
-//TO DO
-//      ADD MORE INFORMATION TO publication_date's CELL. BOOK LENGTH, USER RATING, ETC??
+
 //Growing fond of using "?" as a ternary operator.
 function displayBooks(books){
     const book_table = document.createElement("table");
     books.forEach(book => {
         //As one can see, we are accessing the "volumeInfo" portion of the information we've just received for each individual book. 
         const title = book.volumeInfo.title;
+        const title_link = book.volumeInfo.infoLink;
+        //Some of the fetched books might be missing a cover or other requested information and if we do not use a placeholder image or string in their place, the script will encounter an error.
         const author = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Author';
         const publication_date = book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate: 'Unknown';
         const genres = book.volumeInfo.categories ? book.volumeInfo.categories.join(', '): 'Unknown Genre';
-        //Some of the fetched books might be missing a cover and if we do not use a placeholder image in their place, the script will encounter an error.
         const image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'assets/placeholder_cover.jpg';
         const publisher = book.volumeInfo.publisher;
-
+        const rating = book.volumeInfo.averageRating ? book.volumeInfo.averageRating : "-";
+        const ratingCount = book.volumeInfo.ratingsCount ? book.volumeInfo.ratingsCount : "-";
+        // This creates the rows filled with all the fetched information and possible placeholders in case of errors or missing information and adds them to the "book_table" element, which will eventually be appended to the book_div.
         const row = document.createElement("tr");
-        row.innerHTML = `<td><img src="${image}"></td><td>${title}</td><td>${author}</td><td>${genres}</td><td>${publication_date}<br>${publisher}</td>`;
+
+        row.innerHTML = `<td><img src="${image}"></td><td><a href="${title_link}" target="_blank">${title}</a></td><td>${author}</td><td>${genres}</td><td>${publication_date}<br>${publisher}<br>Rating: ${rating}<br>Reviews: ${ratingCount}</td>`;
+
         book_table.appendChild(row);
     });
-    book_div.innerHTML = ''; // This ensures that possible previous entries will be removed.
+    book_div.innerHTML = ''; // This ensures that possible previous entries are removed.
     book_div.appendChild(book_table);
 }
 
+// We'll use this function to monitor the process and inform the user in case something kicks the bucket during the query. For example, if they use an invalid search term.
+function ErrorMessages() {
+    const inputFields = document.querySelectorAll("#author, #title, #genre");
+    inputFields.forEach(field =>{
+        field.style.borderColor = "red";
+    });
+    const error_message = document.createElement("p");
+    error_message.id = "error_message";
+    error_message.textContent = "Error fetching books. Maybe another search term might do the trick?";
+    error_message.style.color = "red";
+    error_message.style.fontweight = "bold";
+    document.querySelector("#search_form").appendChild(error_message);
+    inputFields.textContent = "";
+}
 
-// function displayBooks(books){
-//     const book_table = document.createElement("div");
-//     books.forEach(book => {
-//         const title = book.volumeInfo.title;
-//         const author = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Author';
-//         console.log(`Title: ${title}, Author(s): ${author}`)
-//     });
-// }
+// This simply goes through the input fields and turns their styles back to the defaults after the user has given an acceptable search term.
+function resetFields(){
+    const inputFields = document.querySelectorAll("#author, #title, #genre");
+    inputFields.forEach(field => {
+        field.style.borderColor = "";
+    });
+}
